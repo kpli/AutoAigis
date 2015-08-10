173023342cf7a3558dc508f2d381fe4425e30205
kpli <vi.m@163.com>
2015-08-10 14:49:33 +0800

    set gaming web tab title for saving screen print

---
+++ playing.js
@@ -0,0 +1,23 @@
+/**
+ * Created by John on 2015/8/7.
+ */
+
+
+function setTabTitle()
+{
+    chrome.storage.local.get("nutakuMailString", function(valueArray) {
+        mailAll = valueArray["nutakuMailString"];
+        document.title = mailAll;
+    });
+}
+
+function main() {
+    var sDomain = window.document.domain;
+    if (sDomain ==  "pf.nutaku.info")
+    {
+        setTimeout("setTabTitle()",1000);
+    }
+
+}
+
+main();
